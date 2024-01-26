fcc1f218cc608f70b125b3244d9b6a14dcc51787
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2024-01-26 14:59:36 +0200

    completely fixed carousel

--- js/index.js
+++ js/index.js
@@ -1,26 +1,37 @@
 $(document).ready(function () {
   $(".arrow").on("click", function () {
-    var currentImg = $(".active");
+    var currentImg = $(".slidein img.active");
     var nextImg = currentImg.next();
-    if (nextImg.length) {
-      currentImg.removeClass().css("z-index", -10);
-      nextImg.addClass("active").css("z-index", 10);
+
+    if (nextImg.length === 0) {
+      nextImg = $(".slidein img:first");
     }
+
+    currentImg.removeClass("active").css("z-index", -10);
+    nextImg.addClass("active").css("z-index", 10);
   });
+
   $(".prev").on("click", function () {
-    var currentImg = $(".active");
-    var prevImg = currentImg.arrow();
-    if (prevImg.length) {
-      currentImg.removeClass().css("z-index", -10);
-      prevImg.addClass("active").css("z-index", 10);
+    var currentImg = $(".slidein img.active");
+    var prevImg = currentImg.prev();
+
+    if (prevImg.length === 0) {
+      prevImg = $(".slidein img:last");
     }
+
+    currentImg.removeClass("active").css("z-index", -10);
+    prevImg.addClass("active").css("z-index", 10);
   });
-  $(".arrow").on("click", function () {
-    var currentSVG = $(".activeSVG");
+
+  $(".next").on("click", function () {
+    var currentSVG = $(".sirc .activeSVG");
     var nextSVG = currentSVG.next();
-    if (nextSVG.length) {
-      currentSVG.removeClass();
-      nextSVG.addClass("activeSVG");
+
+    if (nextSVG.length === 0) {
+      nextSVG = $(".sirc img:first");
     }
+
+    currentSVG.removeClass("activeSVG");
+    nextSVG.addClass("activeSVG");
   });
 });
